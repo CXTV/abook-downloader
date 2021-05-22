0e0f2ed00fc83ecdc14de89467284f254d6fd718
Sam0230 <31580682+Sam0230@users.noreply.github.com>
2021-05-23 03:28:27 +0800

    Add files via upload

---
+++ untitled folder/test.js
@@ -0,0 +1,83 @@
+!{
+	"viewer": 1,
+	"mediaTypeName": "视频",
+	"downloadNumber": 0,
+	"resFileUrl": "5000003478/resourses/2019/9/29/f385b28b-92f7-4d93-a2bb-de57d2dd415e.mp4",
+	"resTitle": "24电题",
+	"picUrl": "/systemImg/builtIn/50video.png",
+	"resAbstract": "暂无资源简介",
+	"downloadDeductScore": 0,
+	"scanNumber": 95,
+	"resourceInfoId": 5000313964,
+	"myresSize": "5MB",
+	"resBrowserUrl": null,
+	"resSortName": "教学录像",
+	"format": "mp4",
+	"path": [
+		"重难点习题讲解",
+		"2. 电化学",
+		"试题24"
+	]
+};
+!{
+	"viewer": 1,
+	"mediaTypeName": "视频",
+	"downloadNumber": 0,
+	"resFileUrl": "5000003478/resourses/2019/9/29/2a29c907-169c-4a5a-a32b-feb176989d06.mp4",
+	"resTitle": "17热题",
+	"picUrl": "/systemImg/builtIn/50video.png",
+	"resAbstract": "暂无资源简介",
+	"downloadDeductScore": 0,
+	"scanNumber": 43,
+	"resourceInfoId": 5000313957,
+	"myresSize": "4MB",
+	"resBrowserUrl": null,
+	"resSortName": "教学录像",
+	"format": "mp4",
+	"path": [
+		"重难点习题讲解",
+		"1. 化学热力学",
+		"试题17"
+	]
+};
+!{
+	"viewer": 1,
+	"mediaTypeName": "视频",
+	"downloadNumber": 0,
+	"resFileUrl": "5000003478/resourses/2019/9/29/5ee11dbb-01b4-4499-a916-5c13f1492f04.mp4",
+	"resTitle": "37动题",
+	"picUrl": "/systemImg/builtIn/50video.png",
+	"resAbstract": "暂无资源简介",
+	"downloadDeductScore": 0,
+	"scanNumber": 19,
+	"resourceInfoId": 5000313977,
+	"myresSize": "2MB",
+	"resBrowserUrl": null,
+	"resSortName": "教学录像",
+	"format": "mp4",
+	"path": [
+		"重难点习题讲解",
+		"3. 化学动力学",
+		"试题37"
+	]
+};
+!{
+	"viewer": 8,
+	"mediaTypeName": "文本",
+	"downloadNumber": 0,
+	"resFileUrl": "5000003478/resourses/2019/9/29/ec9591b9-9aed-49dd-a2cd-6890ae40d581.pptx",
+	"resTitle": "第6章",
+	"picUrl": "/systemImg/builtIn/50ppt.png",
+	"resAbstract": "暂无资源简介",
+	"downloadDeductScore": 0,
+	"scanNumber": 372,
+	"resourceInfoId": 5000313936,
+	"myresSize": "2MB",
+	"resBrowserUrl": null,
+	"resSortName": "电子教案",
+	"format": "pptx",
+	"path": [
+		"电子教案",
+		"第六章"
+	]
+};
