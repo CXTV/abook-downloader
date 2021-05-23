7a15eb0b7f56b490ab720b244af349017eaa16a0
Sam0230 <31580682+Sam0230@users.noreply.github.com>
2021-05-23 22:39:15 +0800

    Update packer.sh

--- packer.sh
+++ packer.sh
@@ -3,7 +3,9 @@ npm i --save-dev rollup --save-dev @rollup/plugin-node-resolve --save-dev @rollu
 npm i progress request string-width performance-now
 npx rollup abook-downloader.js --format cjs -p node-resolve,commonjs,json 2>/dev/null | sed -E 's/^util\$g.inherits\(PrivateKey\$4\, Key\$5\);$//' | npx uglify-js -m eval=true,v8=true -c -e >binary/abook-downloader.bundle.min.js
 npx pkg --public -t node16-mac-arm64 --options expose-gc executeJS.js -o binary/node-mac-arm64
+codesign --remove-signature binary/node-mac-arm64
 npx pkg --public -t node16-mac-x64 --options expose-gc executeJS.js -o binary/node-mac-x64
+codesign --remove-signature binary/node-mac-x64
 npx pkg --public -t node16-windows-arm64 --options expose-gc executeJS.js -o binary/node-windows-arm64.exe
 npx pkg --public -t node16-windows-x64 --options expose-gc executeJS.js -o binary/node-windows-x64.exe
 # npx pkg --public -t node14-windows-x86 --options expose-gc executeJS.js -o node-windows-x86.exe
